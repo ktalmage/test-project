0b96e1384f6200e7938a6c110dfe4dbdda745780
ktalmage <ktalmage85@gmail.com>
2020-05-16 15:31:49 -0400

    added investment route updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    # def current_user
+    #     User.find_by(id: session[:user_id]) 
+    # end
+
+    # def logged_in?
+    #     !current_user.nil?
+    # end
+
+    # def authorized
+    #     redirect_to 'welcome' unless logged_in?
+    # end
 end
--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -3,5 +3,33 @@ class InvestmentsController < ApplicationController
 
     def index
         @user = current_user.email
+        @investments = Investment.all
+    end
+
+    def show
+        find_inv
+        redirect_to user_investment_path
+    end
+
+    def new
+    end
+
+    def create
+    end
+    
+    def edit 
+    end
+
+    def destroy
+    end
+
+    private
+
+    def find_inv
+        @investment = Investment.find_by_id(params[:id])
+    end
+
+    def invest_params
+        params.require(:investment).permit(:name,:type,:income,:expense,:ein)
     end
 end
